451dbfb51f6afd41bf6f74c73f5fc42f96098c75
Anton Zhygariev <anton.zhyharyev@gmail.com>
2020-08-30 14:14:36 +0300

    Create task-2.js

---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+const countProps = function (obj) {
+	"use strict";
+	let values = Object.keys(obj);
+	return values.length;
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ a: 1, b: 1 })); // 2
+
+console.log(countProps({ a: 1, b: 1, c: 1, d: 1, e: 1 })); // 5
